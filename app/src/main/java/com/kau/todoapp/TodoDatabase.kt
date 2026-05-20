8e583611b8c5282893e8956fa2da8cfae3f4902d
kaustubhsatam <kaustubhsatam@hotmail.com>
2026-05-21 01:47:22 +0530

    feat: create TodoDatabase singleton using Room
    - Added Room database configuration
    - Registered Task entity and DAO
    - Implemented singleton database instance
    - Added Room database builder setup

---
+++ app/src/main/java/com/kau/todoapp/TodoDatabase.kt
@@ -0,0 +1,26 @@
+package com.kau.todoapp
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Task::class], version = 1, exportSchema = false)
+abstract class TodoDatabase : RoomDatabase(){
+    abstract class taskDao(): TaskDao
+    companion object{
+        @Volatile
+        private var INSTANCE : TodoDatabase? = null
+
+        fun getDatabase(context: Context): TodoDatabase{
+            return INSTANCE?: synchronized(this){
+                val instance= Room.databaseBuilder(context.applicationContext,
+                    TodoDatabase::class.java,"todo_database").build()
+                INSTANCE = instance
+
+                instance
+            }
+        }
+
+    }
+}
